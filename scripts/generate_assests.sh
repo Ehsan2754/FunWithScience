b2588fdebcf99a59dd7ec978449975ce58dd5964
ehsan2754 <ehsan2754@github.com>
2022-11-18 02:22:39 +0300

    rename to index.html

--- scripts/generate_assests.sh
+++ scripts/generate_assests.sh
@@ -6,10 +6,10 @@ NOTEBOOKS_DIR="notebooks/"
 mkdir -p "$TARGET_DIR.html"
 
 echo "Generating slides from .ipynb files...";
-for FILE in ${NOTEBOOKS_DIR}*.ipynb; do
+for FILE in ${NOTEBOOKS_DIR}.ipynb/*.ipynb; do
     echo "\t[$(date +%T)]\tConverting '${FILE}' to HTML";
     jupyter nbconvert --to slides html  $FILE;
 done;
 echo "Moving generated .html assests";
-mv -v ${NOTEBOOKS_DIR}*.html $TARGET_DIR;
+mv -v ${NOTEBOOKS_DIR}.ipynb/*.html $TARGET_DIR;
 echo "Finished generating";

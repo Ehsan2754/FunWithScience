944119c2298edc273be092e1c9d4ed3bb6251b5b
ehsan2754 <ehsan2754@github.com>
2022-11-18 16:09:46 +0300

    eigen faces notebook added

--- notebooks/.jl/Pendulum.jl
+++ notebooks/.jl/Pendulum.jl
@@ -1,3 +1,4 @@
+# using Markdown
 using ReinforcementLearning #Environments and API
 using Lux #Neural networks with explicit parameterization (Flux would also work) To install: ] add https://github.com/avik-pal/Lux.jl
 using Random #RNG generation
@@ -78,7 +79,14 @@ tr = deepcopy(ps)
 fltr = Flatten.flatten(tr, Array) #Flatten so that we can iterate easily over all parameters in a vector of arrays instead of through the struct
 [x .= 0.0 .* x for x in fltr] #Set initial traces to 0
 
-params = PGParams(0.95, 0.00001, 0.1) #Set learning parameters
+# params = PGParams(0.95, 0.00001, 0.1) #Set learning parameters
+## resulted TotalRewardPerEpisode = ([-1459.8485250348526, -1520.4889067237827, -1501.5157316182567, -1511.603565641842, -1512.9922405983064, -1414.6018622270428, -1501.6212689238146, -1476.6428638954897, -1472.8941659970733, -1508.5743510471932  …  -885.1615314893519, -9.529818544713574, -646.5859876283188, -535.413153608545, -396.45109469156984, -404.72325191669756, -9.792634414999181, -754.3183415063024, -139.81000559188286, -1206.6985960488344], 0.0, true)
+
+# IF learning parameters were different
+params = PGParams(0.8, 0.01, 0.1) #Set learning parameters
+## resulted TotalRewardPerEpisode([-1277.1390033932169, -1427.329064266151, -1652.4770190913018, -1401.7377382024233, -1503.3131388222503, -1542.7829319293237, -1443.2363858624444, -1514.8500784543046, -1658.1327060443086, -1601.3129666646573  …  -1196.7516922751695, -1517.2550441814667, -979.6371078710482, -1548.4847347267703, -1599.3996243158922, -1568.9194036490833, -1503.4107901317818, -1527.6080747503395, -1511.9098108260805, -1551.0292583541518], 0.0, true)
+
+
 agent = PGAgent(model,ps,st,params,fltr, EWMA(-0.0, 0.001)) #Create agent
 
 #Test model and full agent
@@ -90,7 +98,8 @@ agent.θ
 run(
            agent,
            env,
-           StopAfterEpisode(100000),
+        #    StopAfterEpisode(100000),
+           StopAfterEpisode(1000),# much less reward than higher episode numbers
            TotalRewardPerEpisode()
        )
 
